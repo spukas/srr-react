79cccb4f16feec504754faffab1e1b4960d9f689
Linas Spukas <l.spukas@gmail.com>
2018-02-06 08:41:10 +0200

    Add custom axios configuration on the client

--- src/client/client.js
+++ src/client/client.js
@@ -4,16 +4,21 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { renderRoutes } from 'react-router-config';
 import { createStore, applyMiddleware } from 'redux';
+import axios from 'axios';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 
 import Routes from './Routes';
 import reducers from './reducers';
 
+const axiosInstance = axios.create({
+  baseURL: '/api',
+});
+
 const store = createStore(
   reducers,
   window.INITIAL_STATE,
-  applyMiddleware(thunk),
+  applyMiddleware(thunk.withExtraArgument(axiosInstance)),
 );
 
 ReactDOM.hydrate(
