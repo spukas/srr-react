e5e5ceca650d94cd2451be33f323c348e32a896c
Linas Spukas <l.spukas@gmail.com>
2018-02-07 21:16:23 +0200

    Add more links to Header component depending on auth status

--- src/client/components/Header/index.js
+++ src/client/components/Header/index.js
@@ -3,9 +3,22 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 const Header = ({ auth }) => {
+  console.log({ auth });
+  const authButton = () =>
+    auth ? (
+      <a href="/api/logout">Logout</a>
+    ) : (
+      <a href="/api/auth/google">Sign In</a>
+    );
+
   return (
     <div>
       <Link to="/">React SSR</Link>
+      <div>
+        <Link to="/users">Users</Link>
+        <Link to="/admins">Admins</Link>
+        {authButton()}
+      </div>
     </div>
   );
 };
