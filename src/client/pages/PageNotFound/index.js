7db9677a5f2e82539c02f253142651813bd44f10
Linas Spukas <l.spukas@gmail.com>
2018-02-08 19:03:24 +0200

    Add StaticRouter context for catching not found error

--- src/client/pages/PageNotFound/index.js
+++ src/client/pages/PageNotFound/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const PageNotFound = () => (
-  <div className="center-align" style={{ marginTop: '100px' }}>
-    <h3>Sorry, route not found.</h3>
-  </div>
-);
+const PageNotFound = ({ staticContext = {} }) => {
+  staticContext.noFound = true;
+
+  return (
+    <div className="center-align" style={{ marginTop: '100px' }}>
+      <h3>Sorry, route not found.</h3>
+    </div>
+  );
+};
 
 export default { component: PageNotFound };
